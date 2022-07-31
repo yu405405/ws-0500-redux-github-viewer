3b77cf9c2de4041ba98fcc1c59434e805dfc52d7
yu405405 <66393193+you405405@users.noreply.github.com>
2022-07-31 16:50:53 +0900

    見た目の実装

--- src/App.js
+++ src/App.js
@@ -1,10 +1,71 @@
-import logo from './logo.svg';
 import './App.css';
+import './reset.css';
 
 function App() {
+  
+  // const issue = document.getElementById('issue');
+  // const pr = document.getElementById('pr');
+
+  // issue.addEventListener('click', function() {
+  //   issue.classList.remove('current')
+  // })
+
   return (
     <div className="App">
-      <header className="App-header">
+      <header className='header'>
+          <nav className='nav'>
+            <h1>Github Viewer</h1>
+            <ul>
+              <li><a href="">Issue</a></li>
+              <li><a href="">Pull Request</a></li>
+            </ul>
+          </nav>
+      </header>
+      <main className="main">
+        <div className="wrapper">
+          <nav className="tab" >
+            <li className='issue current' id='issue'><a href="">Issue</a></li>
+            <li className='pr' id='pr'><a href="">Pull Request</a></li>
+          </nav>
+          <div className="search_box">
+            <h2 className='ttl'>Issue</h2>
+            <input type="search" placeholder='issue名で検索'/>
+            <div className="btn_area">
+              <div className='btn left'><a href="#">New</a></div>
+              <div className='btn right'><a href="#">Delete</a></div>
+            </div>
+          </div>
+          <div className="contents">
+            <table className='table'>
+              <tr>
+                <th className='check'><input type="checkbox"></input></th>
+                <th className='empty'></th>
+                <th className='status'>ステータス</th>
+                <th className='author'>作成者</th>
+                <th className='date'>作成日付</th>
+                <th className='update'>更新日付</th>
+              </tr>
+              <tr>
+                <td><input type="checkbox"></input></td>
+                <td>A bug in Top Page</td>
+                <td>Open</td>
+                <td></td>
+                <td>07-31-2022</td>
+                <td>07-31-2022</td>
+              </tr>
+              <tr>
+                <td><input type="checkbox"></input></td>
+                <td>A problem of performance in Top Page</td>
+                <td>Open</td>
+                <td></td>
+                <td>07-31-2022</td>
+                <td>07-31-2022</td>
+              </tr>
+            </table>
+          </div>
+        </div>
+      </main>
+      {/* <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
           Edit <code>src/App.js</code> and save to reload.
@@ -17,7 +78,7 @@ function App() {
         >
           Learn React
         </a>
-      </header>
+      </header> */}
     </div>
   );
 }
